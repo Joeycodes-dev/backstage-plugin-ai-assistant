ddd992a698019b888caf4b040b4bca5ce555e6b1
Keenan Du Plessis <keenan.duplessis@nintex.com>
2025-10-28 20:22:24 +0200

    Cleanup and re-ordered buttons.

--- plugins/ai-assistant/src/components/FeedMePage/FeedMePage.tsx
+++ plugins/ai-assistant/src/components/FeedMePage/FeedMePage.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Content, Page } from '@backstage/core-components';
-import {
-  Box,
-  Button,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 import ExpertUserAutoComplete from './ExpertUserAutoComplete';
 
 export const FeedMePage = () => {
   const [value, setValue] = useState('');
-  const [dropdown, setDropdown] = useState('option1');
-
-  //   console.log('Rich Text Editor Value:', value);
 
   return (
     <Page themeId="home">
@@ -54,33 +41,11 @@ export const FeedMePage = () => {
           />
         </Box>
 
-        {/* <Box mb={6}>
-          <FormHelperText>
-            Provide a detailed answer to the question.
-          </FormHelperText>
-        </Box> */}
-
         <Box my={2}>
           <Typography variant="body1" gutterBottom>
             Who should approve this answer?
           </Typography>
           <ExpertUserAutoComplete />
-
-          {/* <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel>Approver</InputLabel>
-            <Select
-              value={dropdown}
-              onChange={e => setDropdown(e.target.value)}
-            >
-              <MenuItem value="option1">Thinus Du Plooy</MenuItem>
-              <MenuItem value="option2">Sean Henderson</MenuItem>
-              <MenuItem value="option3">Anelle Van Zyl</MenuItem>
-            </Select>
-            <FormHelperText>
-              Choose the subject matter expert. Platty can help you identity who
-              to send it to.
-            </FormHelperText>
-          </FormControl> */}
         </Box>
 
         <Box display="flex" justifyContent="flex-end" gap={2} mr={2} mb={2}>
@@ -90,11 +55,12 @@ export const FeedMePage = () => {
         </Box>
 
         <Box display="flex" justifyContent="flex-end" gap={2} mr={2}>
-          <Button variant="contained" color="success">
-            Approve
-          </Button>
           <Button variant="contained" color="error">
             Reject
+          </Button>
+
+          <Button variant="contained" color="success">
+            Approve
           </Button>
         </Box>
       </Content>
